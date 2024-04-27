649728502760aba05545821efaf8019ca04807a0
Bertan Sogutlu <bertansogutlu@gmail.com>
2024-04-27 19:28:54 +0300

    hero tasarimi tamamlandi

--- src/bodyChildComponents/Hero.jsx
+++ src/bodyChildComponents/Hero.jsx
@@ -24,7 +24,16 @@ function Hero() {
 
   return (
     <div className="relative">
-      <SlArrowLeft    onClick={left} className="w-12 h-12 rounded-full cursor-pointer absolute text-white left-10 top-[45%]"/>
+      <div className="flex flex-col gap-11 absolute bottom-[25%] left-48 text-white">
+        <p className="text-base">SUMMER 2020</p>
+        <h2 className="text-6xl font-bold">NEW COLLECTION</h2>
+        <div>
+        <p className="text-xl">We know how large objects will act,</p>
+        <p className="text-xl">but things on a small scale</p>
+        </div>
+        <button className="text-2xl h-14 w-56 rounded-sm bg-[#2DC071]">SHOP NOW</button>
+      </div>
+      <SlArrowLeft    onClick={left} className="w-12 h-12 rounded-full cursor-pointer absolute text-white left-8 top-[40%]"/>
       <div className="flex">
         {photos.map((item, index) => {
           return (
@@ -37,13 +46,13 @@ function Hero() {
           );
         })}
       </div>
-      <SlArrowRight onClick={right} className="w-12 h-12 rounded-full cursor-pointer absolute text-white right-10 top-[45%]"/>
-      <div className="flex absolute bottom-10 left-[45%] gap-1">
+      <SlArrowRight onClick={right} className="w-12 h-12 rounded-full cursor-pointer absolute text-white right-8 top-[40%]"/>
+      <div className="flex absolute w-40 bottom-10 left-0 right-0 m-auto gap-0.5 justify-center">
         {photos.map((item, index) => {
           return (
             <div
               onClick={() => btn(index)}
-              className="bg-white w-16 h-2.5 cursor-pointer"
+              className={index === active ? "bg-white w-16 h-2.5 cursor-pointer" : "bg-white opacity-50 w-16 h-2.5 cursor-pointer"}
             ></div>
           );
         })}
