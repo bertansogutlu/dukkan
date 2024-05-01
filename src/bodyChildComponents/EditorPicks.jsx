f16104a9adef11278d19be306bf4793db98eb63d
Bertan Sogutlu <bertansogutlu@gmail.com>
2024-05-01 19:34:05 +0300

    editor pic's komponenti yerlesimi yapildi

--- src/bodyChildComponents/EditorPicks.jsx
+++ src/bodyChildComponents/EditorPicks.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import React from "react";
 
 function EditorPicks() {
   return (
-    <div>EditorPicks</div>
-  )
+    <div>
+      <div className="w-[72%] m-auto ">
+        <h2>EDITORS'S PICK</h2>
+        <p>Problems trying to resolve the conflict between</p>
+        <div className="flex gap-8">
+          <div
+            className="w-full aspect-square bg-center bg-cover"
+            style={{
+              backgroundImage:
+                "url(" +
+                "https://images.pexels.com/photos/34153/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350" +
+                ")",
+            }}
+          ></div>
+          <div className="w-full flex gap-8">
+            <div
+              className="w-full bg-center bg-cover"
+              style={{
+                backgroundImage:
+                  "url(" +
+                  "https://images.pexels.com/photos/34153/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350" +
+                  ")",
+              }}
+            ></div>
+            <div className="flex flex-col gap-4 w-full">
+              <div
+                className="grow bg-center bg-cover"
+                style={{
+                  backgroundImage:
+                    "url(" +
+                    "https://images.pexels.com/photos/34153/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350" +
+                    ")",
+                }}
+              ></div>
+              <div
+                className="grow bg-center bg-cover"
+                style={{
+                  backgroundImage:
+                    "url(" +
+                    "https://images.pexels.com/photos/34153/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350" +
+                    ")",
+                }}
+              ></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default EditorPicks
+export default EditorPicks;
